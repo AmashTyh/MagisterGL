4439d7c841adf87a8a2c01f14a17f98bf7a48b71
Alexander <a.tonkhonoev@gmail.com>
2018-04-26 00:09:16 +0700

    some fix to model.

--- MagisterGL/MagisterGL/Model/3d/MAGCrossSection.swift
+++ MagisterGL/MagisterGL/Model/3d/MAGCrossSection.swift
@@ -17,40 +17,59 @@ enum PlaneType {
 
 class MAGCrossSection: NSObject {
    
-   var plane: PlaneType
-   var value: Float
+  var plane: PlaneType
+  var value: Float
+  var greater: Bool
+  
+  init(plane: PlaneType,
+       value: Float,
+    greater: Bool)
+  {
+    self.plane = plane
+    self.value = value
+    self.greater = greater
+  }
    
-   init(plane: PlaneType,
-        value: Float)
-   {
-      self.plane = plane
-      self.value = value
-   }
-   
-   func setVisibleToHexahedron(positions:[SCNVector3]) -> HexahedronVisible
-   {
-      switch plane {
-      case .X:
-         if ((positions[0].x <= value) && (positions[1].x > value) || (positions[2].x <= value)  && (positions[3].x > value)) ||
-            ((positions[4].x <= value) && (positions[5].x > value) || (positions[6].x <= value)  && (positions[7].x > value))
-         {
-            return HexahedronVisible.needSection
-         }
-      case .Y:
-         if ((positions[0].y <= value) && (positions[2].y > value) || (positions[1].y <= value)  && (positions[3].y > value)) ||
-            ((positions[4].y <= value) && (positions[6].y > value) || (positions[5].y <= value)  && (positions[7].y > value))
-         {
-            return HexahedronVisible.needSection
-         }
-      case .Z:
-         if ((positions[0].z <= value) && (positions[4].z > value) || (positions[1].z <= value)  && (positions[5].z > value)) ||
-            ((positions[2].z <= value) && (positions[6].z > value) || (positions[3].z <= value)  && (positions[7].z > value))
-         {
-            return HexahedronVisible.needSection
-         }
-      default:
-         return HexahedronVisible.isVisible
+  func setVisibleToHexahedron(positions:[SCNVector3]) -> HexahedronVisible
+  {
+    switch plane {
+    case .X:
+      if ((positions[0].x <= value) && (positions[1].x > value) || (positions[2].x <= value)  && (positions[3].x > value)) ||
+        ((positions[4].x <= value) && (positions[5].x > value) || (positions[6].x <= value)  && (positions[7].x > value)) {
+        return HexahedronVisible.needSection
       }
-      return HexahedronVisible.isVisible
-   }
+      else {
+        if greater {
+          if ((positions[0].x < value) && (positions[1].x < value) && (positions[2].x < value) && (positions[3].x < value) &&
+            (positions[4].x < value) && (positions[5].x < value) && (positions[6].x < value) && (positions[7].x < value)) {
+            return HexahedronVisible.notVisible
+          }
+        }
+        else {
+          if ((positions[0].x > value) && (positions[1].x > value) && (positions[2].x > value) && (positions[3].x > value) &&
+            (positions[4].x > value) && (positions[5].x > value) && (positions[6].x > value) && (positions[7].x > value)) {
+            return HexahedronVisible.notVisible
+          }
+        }
+      }
+    case .Y:
+      if ((positions[0].y <= value) && (positions[2].y > value) || (positions[1].y <= value)  && (positions[3].y > value)) ||
+        ((positions[4].y <= value) && (positions[6].y > value) || (positions[5].y <= value)  && (positions[7].y > value))
+      {
+        return HexahedronVisible.needSection
+      }
+    case .Z:
+      if ((positions[0].z <= value) && (positions[4].z > value) || (positions[1].z <= value)  && (positions[5].z > value)) ||
+        ((positions[2].z <= value) && (positions[6].z > value) || (positions[3].z <= value)  && (positions[7].z > value))
+      {
+        return HexahedronVisible.needSection
+      }
+    }
+    return HexahedronVisible.isVisible
+  }
+  
+  static func isPointBetweenPoints(point: SCNVector3, firstPoint: SCNVector3, secondPoint: SCNVector3) -> Bool
+  {
+    return true
+  }
 }
